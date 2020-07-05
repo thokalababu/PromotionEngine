d177605b180b27a6796c686945658a5839cb7320
thokalababu <thokalababu4net@gmail.com>
2020-07-05 19:55:26 +0530

    Created Items.java class

---
+++ src/com/example/promotion/Item.java
@@ -0,0 +1,62 @@
+package com.example.promotion;
+
+public class Item {
+
+int priceOfA, priceOfB, priceOfC, priceOfD;
+	
+	public Item() {
+		this.setPriceOfA();
+		this.setPriceOfB();
+		this.setPriceOfC();
+		this.setPriceOfD();
+	}
+
+	public int getPriceOfA() {
+		return priceOfA;
+	}
+
+	public void setPriceOfA() {
+		this.priceOfA = 50;
+	}
+
+	public int getPriceOfB() {
+		return priceOfB;
+	}
+
+	public void setPriceOfB() {
+		this.priceOfB = 30;
+	}
+
+	public int getPriceOfC() {
+		return priceOfC;
+	}
+
+	public void setPriceOfC() {
+		this.priceOfC = 20;
+	}
+
+	public int getPriceOfD() {
+		return priceOfD;
+	}
+
+	public void setPriceOfD() {
+		this.priceOfD = 15;
+	}
+	
+	public int getPrice(String s) {
+		
+		if(s.equals("A"))
+			return getPriceOfA();
+		
+		else if(s.equals("B"))
+			return getPriceOfB();
+		
+		else if(s.equals("C"))
+			return getPriceOfC();
+		
+		else if(s.equals("D"))
+			return getPriceOfD();
+		
+		return -1;
+	}
+}
